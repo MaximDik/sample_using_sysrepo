811a1a422929b24f8defbe97ec30a2950052c5a2
Maxim Dik <maxim22f@yandex.ru>
2025-02-13 14:55:02 +0000

    added run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
+ROOT_DIR=$(pwd)
 set -e
+
+rm -rf $ROOT_DIR/third_party/*
+rm .gitmodules
 
 git submodule add https://github.com/CESNET/libyang.git third_party/libyang
 git submodule add https://github.com/sysrepo/sysrepo.git third_party/sysrepo
-
 git submodule update --init --recursive
+
+
+# echo $ROOT_DIR
+# cd third_party/libyang
+# echo $(pwd)
+
+# if [ -d "build" ]; then
+#     echo "############remove dir build LIBYANG!!! ############"
+#     rm -rf build
+#     mkdir build && cd build
+#     cmake ..
+#     make -j$(nproc)
+# else
+#     mkdir build && cd build
+#     cmake ..
+#     make -j$(nproc)
+# fi
+
+# cd $ROOT_DIR/third_party/sysrepo
+# echo $(pwd)
+
+# if [ -d "build" ]; then
+#     echo "############ remove dir build from SYSREPO!!! ############"
+#     rm -rf build
+#     mkdir build && cd build
+#     cmake -DCMAKE_PREFIX_PATH=$ROOT_DIR/third_party/libyang/build ..
+#     make -j$(nproc)
+# else
+#     mkdir build && cd build
+#     cmake -DCMAKE_PREFIX_PATH=$ROOT_DIR/third_party/libyang/build ..
+#     make -j$(nproc)
+#     echo "############ SYSREPO was builded!!! ############"
+# fi
